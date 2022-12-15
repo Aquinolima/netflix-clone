5928e798c5ed403c6948bca19b91c1170fb223da
Thiago Aquino <aquinolima.thiago@gmail.com>
2022-12-15 00:22:54 -0300

    finished html and css

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
-// Write a message to the console.
-console.log('hello world!');
+const express = require("express");
+const path = require("path");
+const port = 3333;
+
+let initialPath = path.join(__dirname, "public");
+
+let app = express();
+app.use(express.static(initialPath));
+
+app.get("/", (req,res)=>{
+    res.sendFile(path.join(initialPath, "index.html"))
+});
+
+app.listen(port, ()=>{
+    console.log(`Server start up on port ${port}!`)
+});
